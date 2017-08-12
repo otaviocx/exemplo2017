515b0b3b565cf48c300bc2a332a8775bdb923c99
otaviocx <otaviocx@otaviocx-Inspiron-5458>
2017-08-12 13:43:23 -0300

    Commit inicial do arquetipo

---
+++ exemplo2017/src/main/java/br/edu/fasam/exemplo2017/App.java
@@ -0,0 +1,13 @@
+package br.edu.fasam.exemplo2017;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
